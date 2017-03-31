00d7bf71298afc3c20f587a9a35e9f3afe393c5d
Rob Cooper <sunscreem@gmail.com>
2017-03-31 15:32:49 +0100

    Added a holding index page

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Demo Project Holding Page</title>
+</head>
+<body>
+	This is a just a 'blank' repo for use when doing demonstrations.
+</body>
+</html>
